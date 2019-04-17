2fc6dd83b8999fe2457d2fb15b3a11d2b44c00b9
Ahmad Ansari <ansari.ahmad@gmail.com>
2019-04-17 17:18:59 +0500

    Movie Service Implementation

--- MovieExplorer/Sources/Logic/NetworkServices/Services/Movie/MovieService.swift
+++ MovieExplorer/Sources/Logic/NetworkServices/Services/Movie/MovieService.swift
@@ -16,4 +16,36 @@ protocol MovieServiceProtocol: class {
 // MARK: - Movie Service
 final class MovieService: Service, MovieServiceProtocol {
     
+    init() {
+        super.init(baseURL: API.baseURL,
+                   servicePath: ServicePath.discoverMovies)
+    }
+    
+    func latestMovies(page: Int, completionHandler: @escaping ServiceResponseHandler<MovieServiceResponse>) {
+        
+        let parameters: [String: Any] = ["sort_by": "primary_release_date.desc",
+                                         "primary_release_date.lte": "2019-04-30",
+                                         Keys.page: page,
+                                         Keys.apiKey: API.apiKey
+        ]
+        
+        self.peformRequest(parameters: parameters) { (response) in
+            if (response == nil || response?.error != nil) {
+                let error = response?.error
+                SLog.error(error as Any)
+                completionHandler(nil, error)
+            } else {
+                if let json: Any = response?.value {
+                    do {
+                        let moviesResponse: MovieServiceResponse = try self.translation.decode(object: json)
+                        completionHandler(moviesResponse, nil)
+                    } catch {
+                        completionHandler(nil, error)
+                    }
+                } else {
+                    completionHandler(nil, ServiceError.unknown)
+                }
+            }
+        }
+    }
 }
