a531c7b75b1e0bc6ea0b018974f820bffeeafe2b
Ahmad Ansari <ansari.ahmad@gmail.com>
2019-04-17 15:54:29 +0500

    Image Extension for placeholder

---
+++ MovieExplorer/Sources/Extensions/UIImage+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  UIImage+Extension.swift
+//  MovieExplorer
+//
+//  Created by Ahmad Ansari on 17/04/2019.
+//  Copyright © 2019 Ahmad Ansari. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIImage {
+    
+    public static var placeholderImage: UIImage? {
+        return UIImage(named: "movie-placeholder")
+    }
+}
