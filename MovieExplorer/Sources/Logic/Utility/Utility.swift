70213380bebf5d195753897ecc77a84b37f5e973
Ahmad Ansari <ansari.ahmad@gmail.com>
2019-04-17 16:02:09 +0500

    Setup Utility and Constants

---
+++ MovieExplorer/Sources/Logic/Utility/Utility.swift
@@ -0,0 +1,35 @@
+//
+//  Utility.swift
+//  MovieExplorer
+//
+//  Created by Ahmad Ansari on 17/04/2019.
+//  Copyright © 2019 Ahmad Ansari. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SwiftyBeaver
+
+//Swift Logger
+let SLog = SwiftyBeaver.self
+
+class Utility: NSObject {
+    
+    // Make init private for singleton
+    private override init() {
+    }
+    
+    // MARK: Default Context
+    static let defaultUtility = Utility()
+    
+    func configureSwiftLogger() {
+        // add console log destinations
+        let console = ConsoleDestination()  // log to Xcode Console
+        
+        // log format : console output to short time, log level & message
+        console.format = Constants.loggerFormat
+        
+        // add the destinations to SwiftyBeaver
+        SLog.addDestination(console)
+    }
+}
