8fd9620b7b350d6abc510f40a6613295f05958bd
Ahmad Ansari <ansari.ahmad@gmail.com>
2019-04-17 16:23:12 +0500

    Network Services Module

---
+++ MovieExplorer/Sources/Logic/NetworkServices/Services/MovieService.swift
@@ -0,0 +1,19 @@
+//
+//  MovieService.swift
+//  MovieExplorer
+//
+//  Created by Ahmad Ansari on 17/04/2019.
+//  Copyright © 2019 Ahmad Ansari. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Movie Service Protocol
+protocol MovieServiceProtocol: class {
+    
+}
+
+// MARK: - Movie Service
+final class MovieService: Service, MovieServiceProtocol {
+    
+}
