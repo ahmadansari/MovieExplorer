43a089d6ee8d17357eee3ef405e4fc93b733931b
Ahmad Ansari <ansari.ahmad@gmail.com>
2019-04-18 18:57:27 +0500

    Movie and Detail Unit Tests Coverage

--- MovieExplorerTests/Modules/Movies/MoviesModuleTests.swift
+++ MovieExplorerTests/Modules/Movies/MoviesModuleTests.swift
@@ -11,18 +11,93 @@ import XCTest
 
 class MoviesModuleTests: XCTestCase {
     
+    var builder: MoviesModuleBuilder!
+    var viewController: MoviesViewController!
+    
+    override func setUp() {
+        super.setUp()
+        
+        builder = MoviesModuleBuilder()
+        let navController = builder.build() as? UINavigationController
+        viewController = navController?.topViewController as? MoviesViewController
+    }
+    
+    override func tearDown() {
+        builder = nil
+        viewController = nil
+        super.tearDown()
+    }
+    
     func testBuildViewController() {
         // given
-        let builder = MoviesModuleBuilder()
-        
-        // when
-        let navController = builder.build() as? UINavigationController
-        let viewController = navController?.topViewController as? MoviesViewController
+        let builder = self.builder
+        let viewController = self.viewController
         
         // then
         XCTAssertNotNil(builder)
-        XCTAssertNotNil(navController)
         XCTAssertNotNil(viewController)
         XCTAssertNotNil(viewController?.viewModel)
+        XCTAssertEqual(viewController?.viewModel?.page, PageInfo.defaultPage, "Invalid Page")
+    }
+    
+    func testConfigure() {
+        //given
+        let viewModel = viewController.viewModel
+        
+        //if
+        viewModel?.configure()
+        
+        //then
+        XCTAssertNotNil(viewModel?.moviesFRC, "Nil Value Found")
+    }
+    
+    func testLoadViewData() {
+        //given
+        let viewModel = viewController.viewModel
+        let loadExpectation = expectation(description: "Load Movie Data")
+        
+        //if
+        viewModel?.configure()
+        viewModel?.loadViewData {
+            viewModel?.moviesFRC.performFetch(completion: { (error) in
+                XCTAssertNil(error, "Fetch Error")
+                let objectCount = viewModel?.moviesFRC.count() ?? 0
+                XCTAssertNotNil(viewModel?.moviesFRC, "Nil Value Found")
+                XCTAssertGreaterThan(objectCount, 0, "No Movies Found")
+                loadExpectation.fulfill()
+            })
+        }
+        
+        waitForExpectations(timeout: 30) { (error) in
+            if let error = error {
+                XCTFail("waitForExpectations Timeout Error: \(error)")
+            }
+        }
+    }
+    
+    func testLoadNextPage() {
+        //given
+        let viewModel = viewController.viewModel
+        let pageExpectation = expectation(description: "Load Next Page Data")
+        
+        //if
+        viewModel?.configure()
+        viewModel?.loadNextPage()
+        viewModel?.moviesFRC.performFetch(completion: { (error) in
+            //then
+            XCTAssertNil(error, "Fetch Error")
+            let objectCount = viewModel?.moviesFRC.count() ?? 0
+            let page = viewModel?.page ?? 0
+            XCTAssertNotNil(viewModel?.moviesFRC, "Nil Value Found")
+            XCTAssertGreaterThan(objectCount, 0, "No Movies Found")
+            XCTAssertGreaterThan(page, 0, "Invalid Page Number")
+            pageExpectation.fulfill()
+        })
+        
+        waitForExpectations(timeout: 30) { (error) in
+            if let error = error {
+                XCTFail("waitForExpectations Timeout Error: \(error)")
+            }
+        }
     }
 }
